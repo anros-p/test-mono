ff4d876e2f7218f0125c28b17f1354533cf74f94
anros-p <37807776+anros-p@users.noreply.github.com>
2024-09-02 11:55:49 +0200

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { WeatherInfo } from "./WeatherInfo";
+
+function App() {
+    const [weather, setWeather] = useState<WeatherInfo[]>();
+
+    useEffect(() => {
+        fetch("http://localhost:5071/weatherforecast")
+            .then(resp => resp.json())
+            .then(data => setWeather(data));
+    }, []);
+
+    return (
+        <div>
+            {weather?.map(w => (
+                <div>
+                    <div>{w.date}</div>
+                    <div>{w.summary}</div>
+                    <div>{w.temperatureC}</div>
+                    <div>{w.temperatureF}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default App;
